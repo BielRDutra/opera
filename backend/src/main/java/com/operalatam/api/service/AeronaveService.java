8bcf5e9b4d6fd5912298666c7b0f04af54a816c4
Gabriel Dutra <bielrds.rodrigues@gmail.com>
2025-10-18 14:24:31 -0300

    Retomandoi o projeto

---
+++ backend/src/main/java/com/operalatam/api/service/AeronaveService.java
@@ -0,0 +1,16 @@
+package com.operalatam.api.service;
+
+public class AeronaveService {
+        
+    public String getAeronave(String nome) {
+        return null;
+    }
+
+    public String criarAeronave(String nome) {
+        return "Aeronave criada";
+    }
+
+    public String deletarAeronave(Long id) {
+        return "Aeronave deletada";
+    }
+}
