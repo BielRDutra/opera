64d95546ffd7e4096b4b12d59570e74675b6773a
Gabriel Dutra <bielrds.rodrigues@gmail.com>
2025-11-25 21:53:26 -0300

    Setor

--- backend/src/main/java/com/operalatam/api/service/SetorService.java
+++ backend/src/main/java/com/operalatam/api/service/SetorService.java
@@ -1,32 +1,23 @@
 package com.operalatam.api.service;
 
-import org.springframework.stereotype.Service;
-
 import com.operalatam.api.model.Setor;
-import com.operalatam.api.repository.SetorRepository;
 
-@Service
 public class SetorService {
-
-    private final SetorRepository setorRepository;
-
-    public SetorService(SetorRepository setorRepository) {
-        this.setorRepository = setorRepository;
+    public String getSetor(String nome) {
+        return null;
     }
 
-    public Setor createSetor(Setor setor) {
-        //Uma funçao pra validar 
-        // Melhorar o retorno para retornar os erros 
-        // Alterar o retorno de outpout para DTO seguindo as boas praticas
-        if (setor.getNome() == null || setor.getNome().isEmpty()) {
-            throw new IllegalArgumentException("O nome do setor não pode ser nulo ou vazio.");
-        }
-        return setorRepository.save(setor);
-
+    public String createSetor(Setor setor) {
+        return "Setor criado";
     }
 
-    public void deleteSetor(Long id) {
-        setorRepository.deleteById(id);
+    public String deleteSetor(Long id) {
+        return "Setor deletado";
+    }
+
+    public Setor createSetor(String nome) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createSetor'");
     }
 
 }
